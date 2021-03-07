f9f0596eaa32f148968facc7bf5562b4ea2847d6
marunaka610 <you@example.com>
2021-03-07 14:34:45 +0000

    エラー検証

--- src/app/query.rs
+++ src/app/query.rs
@@ -22,21 +22,25 @@ impl Query {
   /// # ゲーム情報一覧
   #[graphql(arguments())]
   fn game_infos(ctx: &AppContext) -> Option<Vec<GameInfoDto>> {
+    println!("called qery::game_infos;");
     Some(GameInfoService::find_all(ctx.game_info_service()))
   }
   /// # ゲーム情報
   #[graphql(arguments(id(description = "id of the game")))]
   fn game_info(ctx: &AppContext, id: i32) -> Option<GameInfoDto> {
+    println!("called qery::game_info;");
     Some(GameInfoService::find_unique(ctx.game_info_service(), id))
   }
   /// # ゲーム進捗一覧
   #[graphql(arguments(id(description = "id of game")))]
   fn pregresses(ctx: &AppContext, id: i32) -> Option<Vec<ProgressDto>> {
+    println!("called qery::pregresses;");
     Some(ProgressService::find_all(ctx.game_info_service(), id))
   }
   /// # ゲーム進捗
   #[graphql(arguments(id(description = "id of game")))]
   fn progress_last(ctx: &AppContext, id: i32) -> Option<ProgressDto> {
+    println!("called qery::progress_last;");
     Some(ProgressService::find_unique(ctx.progress_service(), id))
   }
 }
